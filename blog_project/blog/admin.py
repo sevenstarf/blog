ec4fdf885a425210ba6bc8ac0f32ebdf9d10c2c2
sevenstarf <fanfuxing451@163.com>
2018-03-11 20:13:23 -0700

    python

---
+++ blog_project/blog/admin.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+from blog.models import *
+# Register your models here
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title','category','content','pub')
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('blog','name','content','pub')
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Tag,TagAdmin)
+admin.site.register(Blog,BlogAdmin)
+admin.site.register(Comment,CommentAdmin)
